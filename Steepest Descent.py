0d5429360f927d50e5331fe888f34fc81f40c260
Iris <ciciyeung96@gmail.com>
2019-09-16 14:11:47 -0400

    Added python code and images

---
+++ Steepest Descent.py
@@ -0,0 +1,75 @@
+import numpy as np # support for large, multi-dimensional arrays and matrices
+import numpy.linalg as la # Linear algebra
+import matplotlib.pyplot as plt # Useful plotting tool for data visualization
+from mpl_toolkits.mplot3d import axes3d # Plot the 3d graph
+from matplotlib.animation import FuncAnimation
+import matplotlib
+
+def f(x):
+    '''
+    Define the objective function
+    '''
+    return x[0]**2 + x[0]*x[1] + 3*x[1]**2
+
+def df(x):
+    '''
+    Define the gradient of the objective
+    '''
+    return np.array([2*x[0] + x[1], x[0] + 6*x[1]])
+
+# Using axes3d to plot the 3d shape
+
+fig = plt.figure(1)
+ax = fig.gca(projection="3d")
+xmesh, ymesh = np.mgrid[-5:5:50j,-5:5:50j]
+fmesh = f(np.array([xmesh, ymesh]))
+ax.plot_surface(xmesh, ymesh, fmesh)
+
+
+# Plot the contour graph of the function
+plt.figure(2)
+plt.axis("equal")
+plt.contour(xmesh, ymesh, fmesh, 15)
+
+
+guesses = [np.array([3, 3])] # starting point
+count = 0
+while count <= 9:
+#     Calculate the gradient
+    x = guesses[-1] # Always get the newest element from the array
+    g = -df(x) # best improving direction
+    t_opt = 0.1 # step size = 0.1
+    next_guess = x + t_opt * g
+    guesses.append(next_guess)
+    print("The current dot is :", x)
+    print("g =", g)
+#     Plot it
+    plt.figure(3)
+    plt.axis("equal")
+    plt.contour(xmesh, ymesh, fmesh, 15)
+    it_array = np.array(guesses)
+    plt.plot(it_array.T[0], it_array.T[1], "x-") # plot current point
+    count += 1 # increase iteration
+   
+
+plt.show()    
+
+print("Numbers of iterations :", count)
+
+# import numpy as np
+# import matplotlib
+# matplotlib.use('TkAgg')
+# import matplotlib.pyplot as plt
+
+# X=np.linspace(0,100,num=1000)
+# Y=X**2-X
+
+# ranges = [[0, 50], [50, 100]]
+
+# for r in ranges:
+#     cond = (X >= r[0]) & (X <= r[1])
+#     fig=plt.figure()
+#     plt.plot(X[cond],Y[cond])
+#     plt.xlim(r)
+
+# plt.show()
